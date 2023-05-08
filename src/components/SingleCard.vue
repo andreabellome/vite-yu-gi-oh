4aa9677f5143496b8f8148243f5dd6b2043175b6
Andrea Bellome <andrea.bellome@gmail.com>
2023-05-09 01:45:38 +0200

    cards created

--- src/components/SingleCard.vue
+++ src/components/SingleCard.vue
@@ -8,7 +8,7 @@ export default {
 
 <template>
     <div class="card col-12 col-md-6 col-lg-3 mb-2">
-        <img src="datiSingolaCard.card_images[0].image_url" class="card-img-top" alt="...">
+        <img :src="datiSingolaCard.card_images[0].image_url" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">{{ datiSingolaCard.name }}</h5>
             <span class="card-text">{{ datiSingolaCard.archetype }}</span>
